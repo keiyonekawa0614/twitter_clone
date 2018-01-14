6b45fce90916518b64bb0b9fdf0fed3442908520
kei_yonekawa <kei.yonekawa0614@gmail.com>
2018-01-15 00:15:44 +0900

    modify template comment and indent

--- SampleLaravel/resources/views/layouts/my.blade.php
+++ SampleLaravel/resources/views/layouts/my.blade.php
@@ -5,29 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF トークン -->
+    {{-- CSRF トークン --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@if (! Request::is('/')){{ $title }} | @endif{{ env('APP_NAME') }}</title>
 
-    <!-- Bootstrap用CSS -->
+    {{-- Bootstrap用CSS --}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
     <script type="text/javascript" src="{{ asset('js/modal.js') }}"></script>
 </head>
 <body>
-    <!-- グローバルナビ -->
+    {{-- グローバルナビ --}}
     <nav class="navbar navbar-expand-lg navbar-dark" style="padding: initial;background-color:#1da1f2;">
         <div class="container">
-            <!-- アプリ名 -->
+            {{-- アプリ名 --}}
             <a class="navbar-brand" href="{{ url('/posts') }}">
                <img class="logo" src="{{ asset('image/Twitter_Logo_WhiteOnBlue.png') }}" alt="logo" style="width:50px;height:50px;">ホーム
             </a>
-            <!-- メニュー項目 -->
+            {{-- メニュー項目 --}}
             <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-                <!-- 右詰め -->
+                {{-- 右詰め --}}
                 <ul class="navbar-nav my-2 my-lg-0s">
-                    <!-- ログイン・ログアウト -->
+                    {{-- ログイン・ログアウト --}}
                     @guest
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -36,12 +36,12 @@
                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                     @else
-                    <!-- ツイート投稿ボタン -->
+                    {{-- ツイート投稿ボタン --}}
                        <li class="nav-item">
                           <a href="#" id="new-post" class="btn btn-info" style="color: #fff;" data-toggle="modal" data-target="#basicModal">
                               {{ __('ツイート') }}
                            </a>
-                           <!-- ツイート投稿ページ(ポップアップ画面)-->
+                           {{-- start ツイート投稿ページ(ポップアップ画面)--}}
                            <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
                              <div class="modal-dialog">
                                <div class="modal-content">
@@ -70,7 +70,7 @@
                                </div>
                              </div>
                            </div>
-                           <!-- ツイート投稿ページ(ポップアップ画面)-->
+                           {{-- end ツイート投稿ページ(ポップアップ画面)--}}
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="dropdown-user" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -96,12 +96,12 @@
             </div>
         </div>
     </nav>
-    <!-- 個別ページの内容 -->
-    <div class="container mt-3"><!-- 上マージン3を確保 -->
+    {{-- 個別ページの内容 --}}
+    <div class="container mt-3">
         @yield('content')
     </div>
 
-    <!-- Bootstrap用JavaScript -->
+    {{-- Bootstrap用JavaScript --}}
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
--- SampleLaravel/resources/views/posts/index.blade.php
+++ SampleLaravel/resources/views/posts/index.blade.php
@@ -1,51 +1,55 @@
 @php
-    $title = __('Posts');
-
-    if (!empty($postdata)) {
-      $key = true;
-    } else {
-      $key = false;
-    };
-
+$title = __('Posts');
+if (!empty($postdata)) {
+  $key = true;
+} else {
+  $key = false;
+};
 @endphp
 @extends('layouts.my')
 @section('content')
 <div class="table-responsive">
   <div class="container" style="overflow: hidden;">
-	<div class="row" style="margin-right: -195px;margin-left: 195px;">
-  <div class="col-md-8 col-md-offset-2">
-    <div class="panel panel-default">
-      <div class="panel-body" style="padding: 15px;">
-        @if($key)
-        <div class="alert alert-success alert-dismissible" role="alert">
-         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-         <strong>Success!</strong>
-       </div>
-       @endif
+    <div class="row" style="margin-right: -195px;margin-left: 195px;">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+          <div class="panel-body" style="padding: 15px;">
 
-        <form action="{{ url('posts') }}" method="post">
+          {{-- start ツイート投稿成功メッセージ --}}
+          @if($key)
+          <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>Success!</strong>
+          </div>
+          @endif
+          {{-- end ツイート投稿成功メッセージ --}}
+
+          {{-- start ツイート投稿フォーム --}}
+          <form action="{{ url('posts') }}" method="post">
             {{ csrf_field() }}
             {{ method_field('POST') }}
             <div style="margin-bottom:0.3rem;">
                 <textarea id="body" class="form-control" name="body" rows="3" placeholder="いまどうしてる？" required></textarea>
             </div>
-             <button type="submit" name="submit" class="btn btn-info btn-sm" style="margin-bottom:5px;">{{ __('ツイート') }}</button>
-      </form>
-          <div class="table-container">
+            <button type="submit" name="submit" class="btn btn-info btn-sm" style="margin-bottom:5px;">{{ __('ツイート') }}</button>
+          </form>
+          {{-- end ツイート投稿フォーム --}}
 
-							<table class="table table-filter">
-								<tbody>
+          <div class="table-container">
+          <table class="table table-filter">
+              <tbody>
                   @foreach ($posts as $post)
-									<tr data-status="pagado">
-										<td>
+                  <tr data-status="pagado">
+                  <td>
                       <a style="text-decoration: none;color: black;" >
-											<div class="media">
-												<a href="#" style="padding-right: 10px;float: left!important;">
-													<img src="https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg" class="media-photo">
-												</a>
-												<div class="media-body">
+                      <div class="media">
+                        <a href="#" style="padding-right: 10px;float: left!important;">
+                          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg" class="media-photo">
+                        </a>
+                        <div class="media-body">
                           <a style="text-decoration: none;color: black;" href="#" data-toggle="modal" data-target="#detailModal" data-whatever="{{ $post->name }},{{ $post->created_at }},{{ $post->body }}">
-                            <!-- ツイート詳細ページ(ポップ画面表示)-->
+
+                            {{-- start ツイート詳細ページ(ポップ画面表示) --}}
                             <div class="modal fade" id="detailModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
                               <div class="modal-dialog">
                                 <div class="modal-content">
@@ -79,8 +83,9 @@
                                 </div>
                               </div>
                             </div>
-                            <!-- ツイート詳細ページ(ポップ画面表示)-->
-													<span style="float: right!important;" class="media-meta">{{ $post->created_at }}</span>
+                            {{-- end ツイート詳細ページ(ポップ画面表示)--}}
+
+                          <span style="float: right!important;" class="media-meta">{{ $post->created_at }}</span>
 													<h4 class="title">
 														{{ $post->name }}
 														<span style="float: right!important;" class="pagado">{{ $post->name }}</span>
