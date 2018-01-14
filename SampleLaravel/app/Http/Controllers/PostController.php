9771b526da205e7a9995c723831f61f2c9feafd5
kei_yonekawa <kei.yonekawa0614@gmail.com>
2018-01-14 19:13:30 +0900

    modify PostController

--- SampleLaravel/app/Http/Controllers/PostController.php
+++ SampleLaravel/app/Http/Controllers/PostController.php
@@ -21,29 +21,17 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
       if (Auth::check()) {
-        $postdata = Session::get('_old_input');
-        // ログインしている場合、ツイート一覧画面を表示する
-        $id = Auth::id();
-        // 自身のツイートとフォローしているユーザーのツイートを取得する
-        $posts = DB::select('select distinct posts.id, posts.user_id, posts.user_name, posts.body, posts.created_at from posts left outer join follows on posts.user_id = follows.follow_id where follows.user_id = ? or posts.user_id = ? order by posts.created_at desc', [$id, $id]);
-        return view('posts.index', ['posts' => $posts, 'postdata'=>$postdata]);
+        $post = new Post;
+        // ツイート取得
+        $posts = $post->searchPost(Auth::id());
+        // ツイート一覧ページ遷移
+        return view('posts.index', ['posts' => $posts, 'postdata'=>Session::get('_old_input')]);
       } else {
-        // ログインしていない場合、ログインページにリダイレクトする
+        // ログインページにリダイレクト
         return redirect('/login');
       }
-    }
-
-    /**
-     * ツイート投稿画面を表示する
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-         return view('posts.create');
     }
 
     /**
@@ -52,64 +40,10 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request){
       $post = new Post;
-      $post->body = $request->body;
-      $id = Auth::id();
-      $post->user_id = $id;
-      $name = Auth::user()->name;
-      $post->user_name = $name;
-      $post->save();
+      // ツイート登録
+      $post->insertPost($request);
       return redirect('/posts')->withInput();
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        return view('posts.show', ['post' => $post]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        return view('posts.edit', ['post' => $post]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-      $post->title = $request->title;
-      $post->body = $request->body;
-      $post->save();
-      return redirect('posts/'.$post->id);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-      $post->delete();
-      return redirect('posts');
     }
 }
