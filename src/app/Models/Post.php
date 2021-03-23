2e1d1cefe10a70517e993c63ca0e816a9d3ea75b
Kei Yonekawa <kei.yonekawa0614@gmail.com>
2021-03-23 17:34:41 +0900

    fix: post model

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -9,21 +9,33 @@ use Session;
 
 class Post extends Model {
 
-    public function users() {
-      return $this->hasMany('App\Models\User');
-    }
-
-    // ユーザー投稿情報を取得
+    /**
+     * ユーザー投稿情報を取得
+     */
     public static function searchPost($id) {
-      // 自身のツイートとフォローしているユーザーのツイートを取得する
-      return DB::select('select distinct posts.id,posts.user_id,users.name,posts.body,posts.created_at from posts left outer join follows on posts.user_id = follows.follow_id inner join users on users.id = posts.user_id where follows.user_id = ? or posts.user_id = ? order by posts.created_at desc', [$id, $id]);
+      return Post::with('user')
+            ->with(['follow' => function ($query) use ($id) {
+              $query->where('user_id', $id);
+            }])
+            ->orderBy('created_at', 'desc')
+            ->get();
     }
 
-    // ツイート登録
+    /**
+     * ツイート登録
+     */
     public static function insertPost($request) {
       $post = new Post;
       $post->body = $request->body;
       $post->user_id = Auth::id();
       $post->save();
+    }
+
+    public function user() {
+      return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+    public function follow() {
+      return $this->hasMany('App\Models\Follow', 'follow_id');
     }
 }
--- src/resources/views/posts/index.blade.php
+++ src/resources/views/posts/index.blade.php
@@ -15,94 +15,89 @@ if (!empty($postdata)) {
         <div class="panel panel-default">
           <div class="panel-body" style="padding: 15px;">
 
-          {{-- start ツイート投稿成功メッセージ --}}
-          @if($key)
-          <div class="alert alert-success alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <strong>Success!</strong>
-          </div>
-          @endif
-          {{-- end ツイート投稿成功メッセージ --}}
-
-          {{-- start ツイート投稿フォーム --}}
-          <form action="{{ url('posts') }}" method="post">
-            {{ csrf_field() }}
-            {{ method_field('POST') }}
-            <div style="margin-bottom:0.3rem;">
-                <textarea id="body" class="form-control" name="body" rows="3" placeholder="いまどうしてる？" required></textarea>
+            {{-- start ツイート投稿成功メッセージ --}}
+            @if($key)
+            <div class="alert alert-success alert-dismissible" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <strong>Success!</strong>
             </div>
-            <button type="submit" name="submit" class="btn btn-info btn-sm" style="margin-bottom:5px;">{{ __('ツイート') }}</button>
-          </form>
-          {{-- end ツイート投稿フォーム --}}
+            @endif
+            {{-- end ツイート投稿成功メッセージ --}}
 
-          <div class="table-container">
-          <table class="table table-filter">
+            {{-- start ツイート投稿フォーム --}}
+            <form action="{{ url('posts') }}" method="post">
+              {{ csrf_field() }}
+              {{ method_field('POST') }}
+              <div style="margin-bottom:0.3rem;">
+                  <textarea id="body" class="form-control" name="body" rows="3" placeholder="いまどうしてる？" required></textarea>
+              </div>
+              <button type="submit" name="submit" class="btn btn-info btn-sm" style="margin-bottom:5px;">{{ __('ツイート') }}</button>
+            </form>
+            {{-- end ツイート投稿フォーム --}}
+
+            <div class="table-container">
+            <table class="table table-filter">
               <tbody>
-                  @foreach ($posts as $post)
+                @foreach ($posts as $post)
                   <tr data-status="pagado">
-                  <td>
-                      <a style="text-decoration: none;color: black;" >
+                    <td>
                       <div class="media">
                         <a href="#" style="padding-right: 10px;float: left!important;">
                           <img src="{{ asset('image/default_icon.png') }}" class="media-photo">
                         </a>
                         <div class="media-body">
-                          <a style="text-decoration: none;color: black;" href="#" data-toggle="modal" data-target="#detailModal" data-whatever="{{ $post->name }},{{ $post->created_at }},{{ $post->body }}">
-
+                          <a style="text-decoration: none;color: black;" href="#" data-toggle="modal" data-target="#detailModal" data-whatever="{{ $post->user->name }},{{ $post->created_at }},{{ $post->body }}">
                             {{-- start ツイート詳細ページ(ポップ画面表示) --}}
                             <div class="modal fade" id="detailModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
                               <div class="modal-dialog">
                                 <div class="modal-content">
                                   <div class="modal-body">
                                     <div class="table-container">
-                                     <table class="table table-filter">
-                                       <tbody>
-                                         <tr data-status="pagado">
-                  										   <td>
-                                           <a style="text-decoration: none;color: black;" >
-                  											    <div class="media">
-                  												     <a href="#" style="padding-right: 10px;float: left!important;">
-                  													   <img src="{{ asset('image/default_icon.png') }}" class="media-photo">
-                  												     </a>
-                  												  <div class="media-body">
-                  												    	<span style="float: right!important;" class="media-meta">登録日時</span>
-                  													    <h4 class="title">
-                  													  	ユーザー名
-                  														  <span style="float: right!important;" class="pagado">ユーザー名</span>
-                  													    </h4>
-                  													<pre><p class="summary">内容</p></pre>
-                                            </div>
-                  											    </div>
-                                           </a>
-                  										   </td>
-                  									     </tr>
-                                       </tbody>
-                                    </table>
-                                   </div>
+                                      <table class="table table-filter">
+                                        <tbody>
+                                          <tr data-status="pagado">
+                                            <td>
+                                              <a style="text-decoration: none;color: black;" >
+                                                <div class="media">
+                                                  <a href="#" style="padding-right: 10px;float: left!important;">
+                                                    <img src="{{ asset('image/default_icon.png') }}" class="media-photo">
+                                                  </a>
+                                                  <div class="media-body">
+                                                    <span style="float: right!important;" class="media-meta">登録日時</span>
+                                                    <h4 class="title">
+                                                      ユーザー名<span style="float: right!important;" class="pagado">ユーザー名</span>
+                                                    </h4>
+                                                    <pre><p class="summary">内容</p></pre>
+                                                  </div>
+                                                </div>
+                                              </a>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </div>
                                   </div>
                                 </div>
                               </div>
                             </div>
                             {{-- end ツイート詳細ページ(ポップ画面表示)--}}
-
-                          <span style="float: right!important;" class="media-meta">{{ $post->created_at }}</span>
-													<h4 class="title">
-														{{ $post->name }}
-													</h4>
-													<p class="summary">{!! nl2br(e( $post->body )) !!}</p>
+                            <span style="float: right!important;" class="media-meta">{{ $post->created_at }}</span>
+                            <h4 class="title">
+                              {{ $post->user->name }}
+                            </h4>
+                            <p class="summary">{!! nl2br(e( $post->body )) !!}</p>
+                          </a>
                         </div>
-											</div>
-                      </a>
-										</td>
-									</tr>
+                      </div>
+                    </td>
+                  </tr>
                 @endforeach
-								</tbody>
-							</table>
-						</div>
-          </div>
+              </tbody>
+            </table>
+					</div>
         </div>
       </div>
-  </div>
+    </div>
   </div>
 </div>
 @endsection
