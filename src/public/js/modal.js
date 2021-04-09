2b38129e6a5d960f2c5ff2bc54a09d8f12dff784
Kei Yonekawa <kei.yonekawa0614@gmail.com>
2021-04-09 23:05:15 +0900

    fix: delete comment

--- src/public/js/modal.js
+++ src/public/js/modal.js
@@ -2,11 +2,11 @@ window.onload = function(){
 
   // ツイート詳細ページ表示(ポップアップ画面)
   $('#detailModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) //モーダルを呼び出すときに使われたボタンを取得
+    var button = $(event.relatedTarget)
     var whatever = button.data('whatever')
     var resArray = whatever.split(",");
 
-    var modal = $(this)  //モーダルを取得
+    var modal = $(this)
     modal.find('.pagado').text(resArray[0])
     modal.find('.title').text(resArray[0])
     modal.find('.media-meta').text(resArray[1])
@@ -15,10 +15,10 @@ window.onload = function(){
 
   // ユーザーアカウント情報編集ページ表示(ポップアップ画面)
   $('#editModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) //モーダルを呼び出すときに使われたボタンを取得
+    var button = $(event.relatedTarget)
     var username = button.data('username')
     console.log(username)
-    var modal = $(this)  //モーダルを取得
+    var modal = $(this)
     modal.find('.form-control').val(username)
   });
 }
